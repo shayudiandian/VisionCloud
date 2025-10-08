d0e8d2ebb1292e0939b7b01324e03b52c8b82ce5
shayudiandian <kkfw03@163.com>
2025-10-08 14:45:27 +0800

    Add splash layout

---
+++ VisionCloud/app/src/main/java/com/ixuea/courses/mymusic/splash/SplashActivity.kt
@@ -0,0 +1,15 @@
+package com.ixuea.courses.mymusic.splash
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ixuea.courses.mymusic.R
+
+/**
+ * 启动界面
+ */
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
